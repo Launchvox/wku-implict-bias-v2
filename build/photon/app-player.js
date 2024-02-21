39683b072af783d3385e38cdba2a0f49d7208b83
Ian Berget <iberget@launchvox.com>
2024-02-21 06:49:06 -0800

    Submitting build folder for the sake of github pages

---
+++ build/photon/app-player.js
@@ -0,0 +1,23 @@
+var AppPlayer = /** @class */ (function (_super) {
+    __extends(AppPlayer, _super);
+    function AppPlayer(App, name, actorNr, isLocal) {
+        var _this = _super.call(this, name, actorNr, isLocal) || this;
+        _this.App = App;
+        return _this;
+    }
+    AppPlayer.prototype.getId = function () {
+        return this.getCustomProperty("id");
+    };
+    AppPlayer.prototype.getName = function () {
+        return this.getCustomProperty("name");
+    };
+    AppPlayer.prototype.onPropertiesChange = function (changedCustomProps) {
+        this.App.updatePlayerOnlineList();
+    };
+    AppPlayer.prototype.setInfo = function (id, name) {
+        this.App.setUserId(id);
+        this.setCustomProperty("id", id);
+        this.setCustomProperty("name", name);
+    };
+    return AppPlayer;
+}(Photon.LoadBalancing.Actor));
---
+++ build/photon/cloud-app-info.js
@@ -0,0 +1,8 @@
+﻿
+var AppInfo = {
+	Wss: true,
+    AppId: "f91f7a39-e5cd-4f9f-96bf-59b31a9a1a21",
+    AppVersion: "1.0.0",
+//    FbAppId: "you fb app id", 
+}
+
